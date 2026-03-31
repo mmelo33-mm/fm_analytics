c62adc656f888dac6be293d8fbf38ae1fb116ac4
Marcelo Henrique <marcelodemelo605@gmail.com>
2026-03-31 12:19:13 -0300

    .

--- auth.py
+++ auth.py
@@ -9,12 +9,8 @@ import streamlit as st
 
 def conectar():
     return psycopg2.connect(
-        host=st.secrets["DB_HOST"],
-        database=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASSWORD"],
-        port=int(st.secrets["DB_PORT"]),
-        sslmode="require"  
+        st.secrets["DATABASE_URL"],
+        sslmode="require"
     )
 
 # =======================
