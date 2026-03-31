a89c4cc4840227477cebcfcf1db474bd5c3c7ccc
Marcelo Henrique <marcelodemelo605@gmail.com>
2026-03-31 13:04:44 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -12,7 +12,7 @@ def conectar():
         host=st.secrets["DB_HOST"],
         database=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASS"],
+        password=st.secrets["DB_PASSWORD"],
         port=st.secrets["DB_PORT"],
         sslmode="require"
     )
